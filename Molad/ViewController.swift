92be9a31c5c1b42f2d7ad72e9f3424d69e5cc87b
Haskel Ash <haskelash@gmail.com>
2017-04-02 10:51:22 -0700

    create Xcode project

---
+++ Molad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Molad
+//
+//  Created by Haskel Ash on 4/2/17.
+//  Copyright © 2017 HaskelAsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
