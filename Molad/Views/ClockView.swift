6fe337728b00991076ef038a257d353c669736ad
Haskel Ash <haskelash@gmail.com>
2017-04-08 21:49:33 +0300

    add ClockView class

---
+++ Molad/Views/ClockView.swift
@@ -0,0 +1,13 @@
+//
+//  ClockView.swift
+//  Molad
+//
+//  Created by Haskel Ash on 4/2/17.
+//  Copyright © 2017 HaskelAsh. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ClockView: UIView {
+
+}
