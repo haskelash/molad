56c5af7ad088c2a51c131582f5fd9d990b7f9e2f
Haskel Ash <haskelash@gmail.com>
2017-04-08 23:42:05 +0300

    add function to draw chalakim and hours text around clock

--- Molad/Views/ClockView.swift
+++ Molad/Views/ClockView.swift
@@ -47,4 +47,62 @@ import UIKit
         ctx.setLineWidth(border/2)
         ctx.strokePath()
     }
+
+    private func drawText(rect: CGRect, ctx: CGContext, radius: CGFloat) {
+        //flip context for text
+        ctx.translateBy(x: 0, y: rect.height)
+        ctx.scaleBy(x: 1.0, y: -1.0)
+
+        //chalakim text
+        let chalakimPoints = pointsAroundCircle(rect: rect,
+                                                radius: radius*0.82,
+                                                ticks: 24,
+                                                adjustment: .pi/2)
+        let chalakimFont = UIFont(name: "Helvetica Light", size: 8)
+        let chalakimAttrs = [NSFontAttributeName: chalakimFont,
+                             NSForegroundColorAttributeName: UIColor.black] as CFDictionary
+
+        for (i, point) in chalakimPoints.enumerated() {
+            let chalakimStr = "\(i == 0 ? 1080 : i*45)" as CFString
+            let text = CFAttributedStringCreate(nil, chalakimStr, chalakimAttrs)!
+            let line = CTLineCreateWithAttributedString(text)
+            let lineBounds = CTLineGetBoundsWithOptions(line, .useOpticalBounds)
+
+            ctx.setLineWidth(0.5)
+            ctx.setTextDrawingMode(.stroke)
+            let textX = (point.x - lineBounds.midX)
+            let textY = (point.y - lineBounds.midY)
+            ctx.textPosition = CGPoint(x: textX, y: textY)
+
+            CTLineDraw(line, ctx)
+        }
+
+        //hours text
+        let hourPoints = pointsAroundCircle(rect: rect,
+                                            radius: radius*0.68,
+                                            ticks: 24,
+                                            adjustment: .pi/2)
+        let hoursFont = UIFont(name: "Helvetica Light", size: 8)
+        let hoursAttrs = [NSFontAttributeName: hoursFont,
+                          NSForegroundColorAttributeName: UIColor.black] as CFDictionary
+
+        for (i, point) in hourPoints.enumerated() {
+            let hoursStr = "\(i == 0 ? 24 : i)" as CFString
+            let text = CFAttributedStringCreate(nil, hoursStr, hoursAttrs)!
+            let line = CTLineCreateWithAttributedString(text)
+            let lineBounds = CTLineGetBoundsWithOptions(line, .useOpticalBounds)
+
+            ctx.setLineWidth(1)
+            ctx.setTextDrawingMode(.stroke)
+            let textX = (point.x - lineBounds.midX)
+            let textY = (point.y - lineBounds.midY)
+            ctx.textPosition = CGPoint(x: textX, y: textY)
+
+            CTLineDraw(line, ctx)
+        }
+
+        //done with text, flip context back
+        ctx.translateBy(x: 0, y: rect.height)
+        ctx.scaleBy(x: 1.0, y: -1.0)
+    }
 }
